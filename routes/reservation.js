17106c6485a70aa1f060c6133a1df7147262719d
soumaya-elha <57953929+soumaya-elha@users.noreply.github.com>
2020-05-03 18:58:12 +0000

    Update reservation.js

--- routes/reservation.js
+++ routes/reservation.js
@@ -14,13 +14,44 @@ router.get('/reservation', (req, res) => {
 router.post('/reservation', (req, res) => {
 
   var info = req.body;
-  json.push(info);
+  json.push(info); // push---> fichie jison
 
   // saving the array in a file
-  const data = JSON.stringify(json);
+  const data = JSON.stringify(json);// chengement object ---->fichie jison  
   fs.writeFileSync('users.json', data, 'utf-8'); 
 
   res.redirect('/reservation');
+});
+
+//delete
+router.get('/delete/:id', (req, res) => {
+  json = json.filter(d => d.id != req.params.id);
+
+  // saving data
+  const data = JSON.stringify(json);
+  fs.writeFileSync('users.json', data, 'utf-8');
+
+  res.redirect('/reservation')
+});
+
+
+// update
+router.post('/up', (req, res) => {
+  // console.log(req.body, req.params)
+  const { id } = req.body;
+  const { name, prenom, email, telphone ,version } = req.body;
+
+  json.forEach((product, i) => {
+    if (product.id == id) {
+      product.name = name;
+      product.prenom = prenom;
+      product.email = email;
+      product.telphone = telphone;
+      product.version = version;
+    }
+  });
+  res.redirect('/reservation');
+
 });
 
 
