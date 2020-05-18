0e173aa855e75e23b6ddb0dfb71ca8dfcba75af4
soumaya-elha <57953929+soumaya-elha@users.noreply.github.com>
2020-05-18 07:04:56 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,26 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); // objet ---->jison
 const path = require('path');
 const fs = require('fs');
 const ejs = require('ejs');
-const cors = require('cors');
 
-const app = express();
+const app = express();  
 
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views')); //default 'views'
+app.set('view engine', 'ejs'); 
+app.set('views', path.join(__dirname, 'views')); 
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
 
-// Routes
-app.use(require('./routes/galery'));
+
+
 app.use(require('./routes/reservation'));
 
-// 404 handler (ana 7ayadtha)
-// app.use((req, res, next) => {
-//   res.status(404).render('404');
-// });
-
 module.exports = app;
+
+app.listen(3030, () => console.log('server listen port 3030 ...'));
 
 app.listen(3030, () => console.log('server listen port 3030 ...'));
